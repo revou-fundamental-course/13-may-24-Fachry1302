e73fe3ee6b03301f72303ac09835f3ec99b558f5
Fachry  AS <fachry3289@gmail.com>
2024-05-22 01:42:47 +0800

    update6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // Ini JS
+
+document.getElementById('luas-segitiga').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let alas = document.getElementById('alas').value;
+    let tinggi = document.getElementById('tinggi').value; 
+
+    if (alas > 0 && tinggi > 0) {
+        let area = 0.5 * alas * tinggi;
+        document.getElementById('result').innerHTML = ("L = 1/2 x a x t " + "<br>" + "L = 1/2 x " + alas + " x " + tinggi + "<br>" + "L = " + area);
+    } else {
+        document.getElementById('result').innerText = 'Please enter valid positive numbers';
+    }
+});
